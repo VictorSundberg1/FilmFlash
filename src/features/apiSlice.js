efc187816a52ac45c59d53ccd7f505096c496f04
FridaEriksson95 <fridaeriiksson@gmail.com>
2025-10-08 09:28:43 +0200

    Added ui for detailpage

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -3,19 +3,22 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const tmdbApi = createApi({
-	reducerPath: 'tmdbApi',
-	baseQuery: fetchBaseQuery({
-		baseUrl: 'https://api.themoviedb.org/3/',
-		prepareHeaders: (headers) => {
-			headers.set('accept', 'application/json');
-			headers.set('Authorization', `Bearer ${API_KEY}`);
-		},
-	}),
-	endpoints: (build) => ({
-		getTrendingMovies: build.query({
-			query: () => 'trending/movie/day?language=en-US',
-		}),
-	}),
+  reducerPath: 'tmdbApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.themoviedb.org/3/',
+    prepareHeaders: (headers) => {
+      headers.set('accept', 'application/json');
+      headers.set('Authorization', `Bearer ${API_KEY}`);
+    },
+  }),
+  endpoints: (build) => ({
+    getTrendingMovies: build.query({
+      query: () => 'trending/movie/day?language=en-US',
+    }),
+    getMovieDetails: build.query({
+      query: (movieId) => `movie/${movieId}?language=en-US`,
+    }),
+  }),
 });
 
-export const { useGetTrendingMoviesQuery } = tmdbApi;
+export const { useGetTrendingMoviesQuery, useGetMovieDetailsQuery } = tmdbApi;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,29 @@
+import { Link, useNavigate } from 'react-router';
 import { useGetTrendingMoviesQuery } from '../features/apiSlice';
 import './HomePage.css';
 
 function HomePage() {
-	const { data, error, isLoading } = useGetTrendingMoviesQuery();
+  const { data, error, isLoading } = useGetTrendingMoviesQuery();
 
-	const movies = data?.results || [];
+  const movies = data?.results || [];
 
-	if (isLoading) return <h1>Loading..</h1>;
-	if (error) return <h1>Error</h1>;
-	return (
-		<>
-			{movies.map((movie) => (
-				<div key={movie.id}>
+  if (isLoading) return <h1>Loading..</h1>;
+  if (error) return <h1>Error</h1>;
+  return (
+    <>
+      {movies.map((movie) => (
+        /*<div key={movie.id}>
 					<h1>{movie.title}</h1>
-				</div>
-			))}
-			<div>
-				<h1>Content</h1>
-			</div>
-		</>
-	);
+				</div>*/
+        <Link key={movie.id} to={`/details/${movie.id}`}>
+          {movie.title}
+        </Link>
+      ))}
+      <div>
+        <h1>Content</h1>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
