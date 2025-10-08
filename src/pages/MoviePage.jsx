b78dfbfe8902c9e5bf5391b9f472d95bb92748b5
VictorSundberg1 <victor.sundberg1@hotmail.com>
2025-10-08 11:23:33 +0200

    Added frame and moviecards to moviepage

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,19 +1,23 @@
-import { useState } from "react";
-import SearchBar from "../components/SearchBar";
-import "./MoviePage.css";
+import { useState } from 'react';
+import SearchBar from '../components/SearchBar';
+import './MoviePage.css';
+import MoviesFrame from '../components/MoviesFrame';
 
 function MoviePage() {
-  const [movieState, setMovieState] = useState(null);
-  if (movieState?.error) return <h1>Something went wrong...</h1>;
+	const [movieState, setMovieState] = useState(null);
+	if (movieState?.error) return <h1>Something went wrong...</h1>;
 
-  return (
-    <div className='movie-page'>
-      <SearchBar onSearchResult={(state) => setMovieState(state)} />
-      {movieState?.data?.results.map((movie) => (
-        <p key={movie.id}>{movie.title}</p>
-      ))}
-    </div>
-  );
+	const movies = movieState?.data?.results || [];
+
+	return (
+		<div className="movie-page">
+			<SearchBar onSearchResult={(state) => setMovieState(state)} />
+
+			<div className="mainContainer">
+				<MoviesFrame movies={movies} title={'movies'} />
+			</div>
+		</div>
+	);
 }
 
 export default MoviePage;
