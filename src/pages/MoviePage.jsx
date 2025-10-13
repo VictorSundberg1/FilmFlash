28a50fcee4839e65bed04438b62bf0dd4aa21914
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2025-10-13 08:57:00 +0200

    Moved search functionality from SearchBar into MoviePage. Search results now use pagination.

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -2,24 +2,40 @@ import { useEffect, useState } from 'react';
 import SearchBar from '../components/SearchBar';
 import './MoviePage.css';
 import MoviesFrame from '../components/MoviesFrame';
-import { useGetMoviesByGenreQuery } from '../features/apiSlice';
+import {
+	useGetMoviesByGenreQuery,
+	useLazyGetSearchResultQuery,
+} from '../features/apiSlice';
 
 function MoviePage() {
-	const [movieState, setMovieState] = useState(null);
+	const [searchQuery, setSearchQuery] = useState(null);
 	const [activeGenre, setActiveGenre] = useState(28);
 	const [page, setPage] = useState(1);
+
+	const [
+		triggerSearch,
+		{ data: searchData, error: searchError, isLoading: searchIsLoading },
+	] = useLazyGetSearchResultQuery();
 
 	const { data, isLoading } = useGetMoviesByGenreQuery({
 		genreId: activeGenre,
 		page,
 	});
+
 	const genreMovies = data?.results || [];
 
 	// if (movieState?.error) return <h1>Something went wrong...</h1>;
 
 	useEffect(() => {
 		setPage(1);
-	}, [activeGenre, movieState]);
+		setSearchQuery(null);
+	}, [activeGenre]);
+
+	useEffect(() => {
+		if (searchQuery) {
+			triggerSearch({ query: searchQuery, page: page });
+		}
+	}, [page]);
 
 	function incrementPage() {
 		setPage((page) => page + 1);
@@ -29,15 +45,19 @@ function MoviePage() {
 		setPage((page) => Math.max(1, page - 1));
 	}
 
-	const moviesToShow = movieState
-		? movieState?.data?.results || []
-		: genreMovies;
+	const moviesToShow = searchQuery ? searchData?.results || [] : genreMovies;
 
 	return (
-		<div className="movie-page">
-			<SearchBar onSearchResult={(state) => setMovieState(state)} />
+		<div className='movie-page'>
+			<SearchBar
+				onSearch={(query) => {
+					setSearchQuery(query);
+					triggerSearch({ query: query, page: page });
+				}}
+				isLoading={searchIsLoading}
+			/>
 
-			<div className="genreButtons">
+			<div className='genreButtons'>
 				<button onClick={() => setActiveGenre(28)}>Action</button>
 				<button onClick={() => setActiveGenre(35)}>Comedy</button>
 				<button onClick={() => setActiveGenre(18)}>Drama</button>
@@ -45,7 +65,7 @@ function MoviePage() {
 				<button onClick={() => setActiveGenre(10751)}>Family</button>
 			</div>
 
-			<div className="mainContainer">
+			<div className='mainContainer'>
 				{isLoading ? (
 					<p>Loading Movies...</p>
 				) : (
